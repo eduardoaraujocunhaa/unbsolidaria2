8080736201ed25081db23b600107388479c48175
yan <yanvictor_ds@hotmail.com>
2017-01-12 22:35:40 -0200

    import User from models.py to backends.py

--- unbsolidaria/backends.py
+++ unbsolidaria/backends.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-
+from models import User
 class EmailBackend(object):
     def authenticate(self, username=None, password=None):
         if '@' in username:
